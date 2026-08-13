5cd3fa52f587f4c88ce13f5c6f4921003cdb9fee
soportecirnoroeste-dot <soporte.cirnoroeste@gmail.com>
2026-08-13 08:15:56 -0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -97,6 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.head.appendChild(script);
 });
+
 function activarSubmenu(idOpt, btnElement) {
     document.querySelectorAll('.dept-opt-btn').forEach(btn => {
         btn.classList.remove('bg-white', 'shadow-xs', 'text-[#249444]');

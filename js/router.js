7aaa5bea7126f279383847383aed7b4dd958e676
soportecirnoroeste-dot <soporte.cirnoroeste@gmail.com>
2026-08-13 15:10:43 -0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -70,7 +70,8 @@
                     ${iconoCabecera}
                 </div>
                 <div>
-                    <h2 style="font-size: 24px; font-weight: 900; color: #111827; margin: 0 0 4px 0;">${nombreOficialDep.toUpperCase()}</h2>
+                    <h2 style="font-size: 24px; font-weight: 900; color: #111827; margin: 0 0 4px 0;">SISTEMA REGIONAL
+                    INTERNO/ +${nombreOficialDep.toUpperCase()}</h2>
                     <p style="font-size: 14px; color: #4b5563; margin: 0;">${deptoData.subtitle || 'Selecciona una opción para comenzar.'}</p>
                 </div>
             `;

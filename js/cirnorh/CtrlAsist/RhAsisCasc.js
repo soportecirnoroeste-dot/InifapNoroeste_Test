740470f8dfa19da9b5a521bcba2c5b844d11f168
soportecirnoroeste-dot <soporte.cirnoroeste@gmail.com>
2026-09-11 09:18:19 -0700

    Update RhAsisCasc.js

--- js/cirnorh/CtrlAsist/RhAsisCasc.js
+++ js/cirnorh/CtrlAsist/RhAsisCasc.js
@@ -114,7 +114,7 @@ window.RhAsisCasc = {
                                     <div class="flex flex-col gap-1">
 
                                     <div class="flex flex-col gap-1">
-                                        <span class="text-[10px] font-bold text-[#249444] uppercase tracking-wider">Paso 2</span>
+                                        <span class="text-[10px] font-bold text-[#249444] uppercase tracking-wider">Paso 3</span>
                                         <button id="labelCargaDatos" class="px-4 py-2 bg-[#249444] text-white rounded-xl text-xs font-bold hover:bg-[#1e7a37] transition flex items-center gap-2 cursor-pointer shadow-xs h-[34px]" onclick="document.getElementById('uploadBiometrico').click();">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-up"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M12 12v6"/><path d="m15 15-3-3-3 3"/></svg>
                                             <span id="textoCargaBtn">Carga de Datos</span>

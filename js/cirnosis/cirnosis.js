8339c092cc689176f5514e4d8155c0093b8fd245
soportecirnoroeste-dot <soporte.cirnoroeste@gmail.com>
2026-09-15 13:05:55 -0700

    Update cirnosis.js

--- js/cirnosis/cirnosis.js
+++ js/cirnosis/cirnosis.js
@@ -34,13 +34,12 @@ window.cirnosisConfig = {
             icon: "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z'/><path d='M14 2v5a1 1 0 0 0 1 1h5'/><path d='M10 9H8'/><path d='M16 13H8'/><path d='M16 17H8'/></svg>", 
             action: "cargarFormatosSis()" 
         },
-        // Dentro de options en cirnosisConfig:
-{ 
-    id: "permisos", 
-    title: "Permisos", 
-    icon: "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12 2a10 10 0 1 0 7.54 16.63'/><path d='M12 6v6l4 2'/></svg>", 
-    action: "cargarPermisosSis()" 
-}
+        { 
+            id: "permisos", 
+            title: "Permisos", 
+            icon: "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-user-round-key'><path d='M19 11v6'/><path d='M19 13h2'/><path d='M2 21a8 8 0 0 1 12.868-6.349'/><circle cx='10' cy='8' r='5'/><circle cx='19' cy='19' r='2'/></svg>", 
+            action: "cargarPermisosSis()" 
+        }
     ]
 };
 

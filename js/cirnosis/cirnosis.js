572db84f8f69150249fdfc610c27e3ff5b3cff95
soportecirnoroeste-dot <soporte.cirnoroeste@gmail.com>
2026-09-15 11:29:58 -0700

    Update cirnosis.js

--- js/cirnosis/cirnosis.js
+++ js/cirnosis/cirnosis.js
@@ -37,7 +37,7 @@ window.cirnosisConfig = {
         { 
             id: "permisos", 
             title: "Permisos", 
-            icon: "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12 2a10 10 0 1 0 7.54 16.63'/><path d='M12 6v6l4 2'/></svg>", 
+            icon: "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-user-round-key'><path d='M19 11v6'/><path d='M19 13h2'/><path d='M2 21a8 8 0 0 1 12.868-6.349'/><circle cx='10' cy='8' r='5'/><circle cx='19' cy='19' r='2'/></svg>", 
             action: "cargarPermisosSis()" 
         }
     ]

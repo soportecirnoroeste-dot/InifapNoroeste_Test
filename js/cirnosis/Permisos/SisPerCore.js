d7655552a75e8d64af26d286bce06ac203603e90
soportecirnoroeste-dot <soporte.cirnoroeste@gmail.com>
2026-09-17 14:20:28 -0700

    Update SisPerCore.js

--- js/cirnosis/Permisos/SisPerCore.js
+++ js/cirnosis/Permisos/SisPerCore.js
@@ -34,6 +34,13 @@ function renderizarListadoPermisosSis() {
         // Clases idénticas al contenedor raíz del módulo Personal
         contenedorDinamico.className = "w-full space-y-6 bg-white p-6 rounded-xl border border-stone-200 shadow-sm";
         contenedorDinamico.innerHTML = `
+            <!-- Encabezado del Módulo -->
+            <div class="flex items-center gap-3 pb-2 border-b border-stone-100">
+                <div class="p-2.5 bg-[#f0fdf4] border border-[#c6f6d5] text-[#059669] rounded-xl flex items-center justify-center">
+                <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-user-round-key'><path d='M19 11v6'/><path d='M19 13h2'/><path d='M2 21a8 8 0 0 1 12.868-6.349'/><circle cx='10' cy='8' r='5'/><circle cx='19' cy='19' r='2'/></svg>                </div>
+                <h3 class="font-black text-stone-800 text-lg uppercase tracking-wide">Permisos</h3>
+            </div>
+            
             <!-- Bloque superior de gestión -->
             <div id="contenedor-gestion-permisos" class="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 bg-stone-50 p-4 rounded-xl border border-stone-200">
                 <div>
